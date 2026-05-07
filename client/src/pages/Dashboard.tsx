e10129f30a502011412ca2ed93215ab55282ec8f
SANJAY B <sanjay300333@gmail.com>
2026-05-07 13:06:48 +0530

    fix: correct closing tag in Dashboard.tsx

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -122,7 +122,7 @@ const Dashboard: React.FC = () => {
             </div>
             <FileUpload onUploadComplete={fetchDocuments} />
           </section>
-        </div>
+        </aside>
       </div>
     </div>
   );
